323c7f8a8f6bd615245304cfc6e194d744bf3ea7
e601201 <k.arthur1111@gmail.com>
2025-01-15 13:17:56 +0900

    2025/1/15

--- decorator.ts
+++ decorator.ts
@@ -6,13 +6,21 @@ function Logging(message: string) {
   };
 }
 function Component(template: string, selector: string) {
-  return function (constructor: { new (...args: any[]): { name: string } }) {
-    const mountedElement = document.querySelector(selector);
-    const instance = new constructor();
-    if (mountedElement) {
-      mountedElement.innerHTML = template;
-      mountedElement.querySelector("h1")!.textContent = instance.name;
-    }
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    constructor: T
+  ) {
+    return class extends constructor {
+      constructor(...args: any[]) {
+        super(...args);
+        console.log("Component decorator");
+        const mountedElement = document.querySelector(selector);
+        const instance = new constructor();
+        if (mountedElement) {
+          mountedElement.innerHTML = template;
+          mountedElement.querySelector("h1")!.textContent = instance.name;
+        }
+      }
+    };
   };
 }
 
@@ -20,7 +28,12 @@ function Component(template: string, selector: string) {
 @Logging("Logging User")
 class User {
   name = "Quill";
+  age = 38;
   constructor() {
     console.log("User was created!");
   }
 }
+
+const user1 = new User();
+const user22 = new User();
+const user32 = new User();
